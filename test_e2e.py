7cf064cc22177723fdb605f9d60e2f3bf2e03fd4
TaleOfScripting <kyablood@gmail.com>
2023-04-12 18:21:17 -0500

    Config.yml change

--- test_e2e.py
+++ test_e2e.py
@@ -9,7 +9,11 @@ from selenium.webdriver.chrome.service import Service
 @pytest.fixture(scope="module")
 def browser():
     service = Service(executable_path='/chrome_driver/chromedriver')
-    driver = webdriver.Chrome(service=service)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome(service=service, options=chrome_options)
     yield driver
     driver.quit()
 
