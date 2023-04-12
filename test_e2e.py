960872d78a99667154d64c7a190a8fa8049694ee
TaleOfScripting <kyablood@gmail.com>
2023-04-12 18:13:35 -0500

    Selenium Update

---
+++ test_e2e.py
@@ -0,0 +1,28 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+from selenium.webdriver.chrome.service import Service
+
+
+@pytest.fixture(scope="module")
+def browser():
+    service = Service(executable_path='/chrome_driver/chromedriver')
+    driver = webdriver.Chrome(service=service)
+    yield driver
+    driver.quit()
+
+def test_bmi_calculation(browser):
+    browser.get('http://localhost:5000/bmi')
+
+    height_input = browser.find_element(By.ID, 'height')
+    weight_input = browser.find_element(By.ID, 'weight')
+    submit_button = browser.find_element(By.CSS_SELECTOR, 'button[type="submit"]')
+
+    height_input.send_keys('5 7')
+    weight_input.send_keys('150')
+    submit_button.click()
+
+    result_text = browser.find_element(By.CSS_SELECTOR, '.result').text
+    assert result_text == 'Your BMI is: 24.06 and you are of normal weight'
